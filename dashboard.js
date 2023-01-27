840c585fe29b7be9234c4c9591c0f120684379e9
Temótio Luis Bernardo <timotio.bernardo@snowmanlabs.com>
2023-01-27 17:17:13 -0300

    altera as atividades para um estado generico

--- dashboard.js
+++ dashboard.js
@@ -11,7 +11,7 @@ function time() {
     document.getElementById('txt').innerHTML = `${addZero(h)}:${addZero(m)}:${addZero(s)}`;
     document.getElementById('datas').innerHTML = `
         ${addZero(today.getDate())}/
-        ${addZero(today.getMonth())}/
+        ${addZero(today.getMonth() + 1)}/
         ${addZero(today.getFullYear())}
         `
     tarefa0 = document.getElementById('tarefa0')
@@ -61,3 +61,32 @@ function time() {
     }
     setTimeout('time()', 500);
 }
+
+const tasks = [
+    `  <strong id="tarefa0">
+        Cozinha, todos os banheiros e a calçada.
+        </strong>
+    `,
+    `   <strong id="tarefa1">
+        Casa, todos lixos, roupas,
+        panos e os lixos da cozinha.
+        </strong>
+    `
+]
+
+function getTasks() {
+    const containers = document.querySelectorAll('.lista')
+    const currentdate = new Date()
+    const oneJan = new Date(currentdate.getFullYear(), 0, 1);
+    const numberOfDays = Math.floor((currentdate - oneJan) / (24 * 60 * 60 * 1000));
+    const weekNumber = Math.ceil((currentdate.getDay() + 1 + numberOfDays) / 7);
+    //verifica se o weekNumber é par
+    if ((weekNumber % 2) === 0) {
+        containers[0].innerHTML = tasks[1]
+        containers[1].innerHTML = tasks[0]
+    } else {
+        containers[0].innerHTML = tasks[0]
+        containers[1].innerHTML = tasks[1]
+    }
+}
+getTasks()
