5f5c8f0e7f1afa1ff017377444a4a8d54d012ec1
Temotio Luis <temotio.bernardo@snowmanlabs.com>
2023-02-16 15:48:04 -0300

    somando os campos

--- Reports/reports.js
+++ Reports/reports.js
@@ -14,12 +14,39 @@ function submit() {
     var valortotal = document.getElementById('valorTotal').value;
 
     // PEGANDO OS CAMPOS DOS INNERHTML
-    var pessoa1 = document.getElementById('resumoDoValor');
-    var valor1 = document.getElementById('resumoPessoa1');
-    var pessoa2 = document.getElementById('resumoPessoa2');
+    var simulatorValortotal = document.getElementById('resumoDoValor');
+    var valor1Pessoa = document.getElementById('resumoPessoa1');
+    var valorPessoa2 = document.getElementById('resumoPessoa2');
 
-    const porcentagem1 = (Number(valor1) * Number(valortotal))/100
-    console.log(new Number(porcentagem1))
-    alert(`result:', ${porcentagem1}, '%'`)
-    
+    var porcentagem1 = ((valor1) * (valortotal)) / 100
+    var porcentagem2 = ((valor2) * (valortotal)) / 100
+    if ((new Number(porcentagem1) + new Number(porcentagem2)) < new Number(valortotal)) {
+        if ((valor1 == 100 || valor1 < 100)
+            &&
+            (valor2 == 100 || valor2 < 100)
+        ) {
+            simulatorValortotal.innerHTML = `Sabendo que o valor total foi R$ ${valortotal}`
+            valor1Pessoa.innerHTML = `
+            ${pessoa1} 
+            enviou ${valor1}% de <span class='result'>R$ ${valortotal}</span>, 
+            cujo o seu valor nela é: 
+            R$ ${porcentagem1}
+        `
+        valorPessoa2.innerHTML = `
+            ${pessoa2} 
+            enviou ${valor2}% de <span class='result'>R$ ${valortotal}</span>, 
+            cujo o seu valor nela é: 
+            R$ ${porcentagem2}
+        `
+        } else {
+            alert('Valor persentual estáacima de 100%')
+        }
+    } else {
+        alert(`
+        A soma do valor do 
+        ${pessoa1} e do ${pessoa2} 
+        equivale a ${new Number(valor1) + new Number(valor2)},
+        comparado a valor total ${valortotal}, nisso
+        passa dos limites, por isso a conta é rejeitada ):`)
+    }
 }
